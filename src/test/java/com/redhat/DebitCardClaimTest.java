e05e83838065f52a91872dc6df9536db34ebf5b4
Rishijeet Mishra <rishijeet@gmail.com>
2025-05-21 19:25:00 +0530

    adding few changes

--- src/test/java/com/redhat/DebitCardClaimTest.java
+++ src/test/java/com/redhat/DebitCardClaimTest.java
@@ -1,28 +1,21 @@
 package com.redhat;
 
 import org.junit.jupiter.api.Test;
-import org.kie.dmn.api.core.DMNContext;
-import org.kie.dmn.api.core.DMNResult;
-import org.kie.dmn.api.core.DMNRuntime;
-import org.kie.dmn.core.util.KieHelper;
 import io.restassured.http.ContentType;
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.equalTo;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DebitCardClaimTest {
 
     @Test
     public void testDebitCardClaim() {
         given()
-            .contentType(ContentType.JSON)
-            .body("{\"Claim\": {\"claimTimely\": true, \"highRiskTransaction\": false}}")
-        .when()
-            .post("/debit-card-claim")
-        .then()
-            .statusCode(200)
-            .body("'Claim Approval Decision'", equalTo("APPROVED"));
+                .contentType(ContentType.JSON)
+                .body("{\"Claim\": {\"claimTimely\": true, \"highRiskTransaction\": false}}")
+                .when()
+                .post("/debit-card-claim")
+                .then()
+                .statusCode(200)
+                .body("'Claim Approval Decision'", equalTo("APPROVED"));
     }
 }
